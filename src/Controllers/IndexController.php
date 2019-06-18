88c398459f17fb1c39d120cbc0ef3a1521df0b1b
dasha <frgyvu@gmail.com>
2019-06-18 11:33:23 +0300

    ++

---
+++ src/Controllers/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+class IndexController
+{
+    public function indexAction(){
+        echo "Генерация главной страницы";
+        $content='main.php';
+        $template='template.php';
+        $data=[
+            'title'=>'Главная'
+        ];
+        //вывели страничку $page
+        echo $this->renderPage($content,$template,$data);
+    }
+    public function renderPage($content,$template,$data=[]){
+        extract($data);
+        //преобразует массив к виду переменна($ключ = "значение") $title='Главная';
+        ob_start();
+        include_once __DIR__.'/../Views/'.$template;
+        //перетащили считай все содержимое template.php
+        // и весь этот треш гладется строкой в переменную $page
+        $page=ob_get_contents();
+        ob_end_clean();
+        return $page;
+    }
+
+}
