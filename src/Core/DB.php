d4dd52afc539e00c2df47ddeed3aabf7f6625db8
dasha <frgyvu@gmail.com>
2019-06-22 16:32:49 +0300

    ++

--- src/Core/DB.php
+++ src/Core/DB.php
@@ -4,6 +4,8 @@ namespace Web\FrontController\Core;
 
 class DB
 {
+    // данные, необходимые для подключения к базе данных
+    // TODO: вынести в config файл
     private $server = 'localhost';
     private $dbName = 'frontcontroller';
     private $username = 'root';
@@ -12,19 +14,23 @@ class DB
     private static $db; // в данном свойстве будем хранить объект DB класса
     private $connection;
 
+    // делаем конструктор приватным, чтобы объект можно было создать только внутри класса
     private function __construct()
     {
+        // при создании объекта DB в $connection сохранится объект pdo (его мы будем использовать для соединения с бд)
         $this->connection = new \PDO(
-            "mysql:host=$this->server;dbname=$this->dbName",
-            $this->username, $this->pwd,
-            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+            "mysql:host=$this->server;dbname=$this->dbName", $this->username, $this->pwd, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
     }
 
+    // метод создает в случае необходимости объект DB класса или возвращает уже созданный объект
+    // при таком подходе будет создать лишь один объект DB на всю программу
     public static function getDB(){
         if (self::$db == null) self::$db = new DB();
         return self::$db;
     }
 
+    // для запросов вида SELECT * FROM Table;
+    // метод вернет все записи из таблицы по запросу $sql, метод вернет данные, как массив объектов класса $class
     public function getAll($sql, $class){
         $statement = $this->connection->query($sql);
         if (!$statement) return false;
@@ -32,12 +38,18 @@ class DB
         return $statement->fetchAll();
     }
 
+    //для запросов вида DELETE FROM table WHERE id=:id;
+    // метод выполнит запрос подготовленный $sql, передав в него параметры $params
     public function nonSelectQuery($sql, $params){
         $statement = $this->connection->prepare($sql);
         if (!$statement) return false;
         return $statement->execute($params);
     }
 
+
+    //для запросов вида SELECT * FROM Table WHERE age > :age; для получения нескольких записей
+    // метод выполнит запрос подготовленный $sql, передав в него параметры $params,
+    //метод вернет данные, как массив объектов класса $class
     public function paramsGetAll($sql, $params, $class) {
         $statement = $this->connection->prepare($sql);
         if (!$statement) return false;
@@ -46,6 +58,9 @@ class DB
         return $statement->fetchAll();
     }
 
+    //для запросов вида SELECT * FROM Table WHERE id=:id; для получения нодной записи
+    // метод выполнит запрос подготовленный $sql, передав в него параметры $params,
+    //метод вернет данные, как объект класса $class
     public function paramsGetOne($sql, $params, $class){
         $statement = $this->connection->prepare($sql);
         if (!$statement) return false;
--- src/Core/MainRepository.php
+++ src/Core/MainRepository.php
@@ -3,38 +3,42 @@ namespace Web\FrontController\Core;
 
 use Web\FrontController\Core\Repository;
 
+// родительский класс для всех классов - репозиториев
 class MainRepository implements Repository
 {
-    // методы на
-    // добавление записи
-    // получение записи по первичному ключу
-    // получение всех записей
-    // (общие для всех репозиториев)
+    // методы (общие для всех репозиториев) на
+    // добавление записи в таблицу
+    // получение записи по первичному ключу и таблицы
+    // получение всех записей из таблицы
+    // удаление записи из таблице
+    // обновление записи в таблице
+    // Остальные методы будут индивидуальны для каждого класса - репозитория, например, поиск по названию, дате, join запросы и тд
+
+
     protected $db;
     private $class;
 
     public function __construct($class)
     {
-        $this->db = DB::getDB();
-        $this->class = $class;
+        $this->db = DB::getDB(); // объект класса DB для работы с базой данных
+        $this->class = $class; // имя класса - сущности, с которым работает рапозиторий (имя должно соответствовать таблицу в бд)
     }
 
     public function getAll()
     {
-        // $class соответствует таблице в бд
-        // например, Picture
-        // Web\FrontController\Models\Picture
-        // Picture
-        $sql = 'SELECT * FROM ' .
-            basename(str_replace('\\', '/', $this->class));
+        // Например, имя класса ($this->class) Web\FrontController\Models\Picture, значит, мы работаем с таблицей Picture
+        // формируем строку запроса. basename вернет Picture из полного имени класса
+        $sql = 'SELECT * FROM ' . basename(str_replace('\\', '/', $this->class));
+        // вызываем метод getAll() у объекта класса DB, передаем строку запроса и имя класса (см. метод getAll в классе DB)
+        // и возвращаем полученный результат
         return $this->db->getAll($sql, $this->class);
     }
 
     public function getById(int $id)
     {
-        $sql = 'SELECT * FROM ' .
-            basename(str_replace('\\', '/', $this->class)) .
-            ' WHERE id=:id';
+        // вызываем метод getAll() у объекта класса DB, передаем строку запроса, массив с параметрами и имя класса (см. метод paramsGetOne в классе DB)
+        // и возвращаем полученный результат
+        $sql = 'SELECT * FROM ' . basename(str_replace('\\', '/', $this->class)) . ' WHERE id=:id';
         return $this->db->paramsGetOne($sql, ['id'=>$id], $this->class);
     }
 }
