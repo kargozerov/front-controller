bedb9d367794ac277c819d9caeb23f240b89f3d2
dasha <frgyvu@gmail.com>
2019-06-27 16:19:48 +0300

    ++

---
+++ src/Views/admin_account.php
@@ -0,0 +1,25 @@
+<h2>Личный кабинет администратора (художника)</h2>
+
+
+<ul class="nav nav-tabs" id="myTab" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#addpicture" role="tab" aria-controls="home" aria-selected="true">Добавить картину</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#addArticle" role="tab" aria-controls="profile" aria-selected="false">Добавить статью</a>
+    </li>
+</ul>
+<div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="addpicture" role="tabpanel" aria-labelledby="home-tab">
+<!--        Форма добавления картины-->
+        <div class="margin-50 offset-3 col-6">
+            <?php require_once "add_picture.php"?>
+        </div>
+    </div>
+    <div class="tab-pane fade" id="addArticle" role="tabpanel" aria-labelledby="profile-tab">
+        Форма добавления статьи
+    </div>
+</div>
+
+
+
---
+++ src/Views/picture.php
@@ -0,0 +1,6 @@
+<div>
+    <h2><?php echo $picture['title']?></h2>
+    <h2><?php echo $picture['description']?></h2>
+    <p><?php echo $picture['yearCreated']?></p>
+    <p><img src="/img/<?php echo $picture['img']?>"></p>
+</div>
