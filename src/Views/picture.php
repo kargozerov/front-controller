9047ea2de5cad5e3fd79f8e2f65854013fa49125
ifmo-web-developer <limacosha@gmail.com>
2019-06-29 09:11:54 +0300

    Update picture.php

--- src/Views/picture.php
+++ src/Views/picture.php
@@ -1,6 +1,26 @@
-<div>
-    <h2><?php echo $picture['title']?></h2>
-    <h2><?php echo $picture['description']?></h2>
-    <p><?php echo $picture['yearCreated']?></p>
-    <p><img src="/img/<?php echo $picture['img']?>"></p>
+<div class="row margin-50">
+    <div class="col-7">
+        <h1>
+            <?php echo $picture['title']?>
+        </h1>
+        <p><?php echo $picture['yearCreated']?> год</p>
+    </div>
+
+    <div class="col-5">
+        <p class="price">Стоимость: <span><?php echo $picture['price']?></span> руб.</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-7">
+        <img src="/img/<?php echo $picture['img']?>">
+    </div>
+    <div class="col-5">
+        <?php if($auth):?>
+        <p  class="text-center"><a class="btn btn-block btn-warning" href="/cart/addPicture/<?php echo $picture['id']?>">Заказать</a></p>
+        <?php else: ?>
+        <p>Авторизуйтесь, чтобы заказать картину</p>
+        <?php endif; ?>
+        <p><?php echo $picture['description']?></p>
+    </div>
 </div>
